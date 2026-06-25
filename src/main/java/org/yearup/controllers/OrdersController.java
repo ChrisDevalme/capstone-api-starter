8eb4cf55cc13996d7a0bf574abb7ec8062d15fdd
Chris Devalme <cdevalme@gmail.com>
2026-06-24 21:42:15 -0400

    Phase 5 OrdersController

--- src/main/java/org/yearup/controllers/OrdersController.java
+++ src/main/java/org/yearup/controllers/OrdersController.java
@@ -1,4 +1,40 @@
 package org.yearup.controllers;
 
-public class OrdersController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.models.Order;
+import org.yearup.models.User;
+import org.yearup.service.OrderService;
+import org.yearup.service.UserService;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/orders")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class OrdersController
+{
+    private final OrderService orderService;
+    private final UserService userService;
+
+    public OrdersController(OrderService orderService, UserService userService)
+    {
+        this.orderService = orderService;
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> checkout(Principal principal)
+    {
+        String username = principal.getName();
+
+        User user = userService.getByUserName(username);
+        int userId = user.getId();
+
+        Order order = orderService.checkout(userId);
+
+        return ResponseEntity.status(201).body(order);
+    }
 }
